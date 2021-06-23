5ccabfe1ff896aef22172c8d8a5b00ea12797fff
simba-fs <simba.fs@gmail.com>
2021-06-23 11:04:34 +0800

    Info widget

--- component/WeatherDisplay/widget/Info/index.tsx
+++ component/WeatherDisplay/widget/Info/index.tsx
@@ -9,19 +9,27 @@ interface IInfoProps {
 
 import style from './style.module.css';
 
+const addColon = (time: string) => time.slice(0, 2) + ':' + time.slice(2, 4);
+
+const temperature = (temp: string) => temp + '°C';
+
+const humi = (humidity: string) => parseFloat(humidity) * 100 + '%';
+
+const pres = (pressure: string) => pressure + 'hPa';
+
 export default function Info({ humidity, maxTemp, maxTempTime, minTemp, minTempTime, pressure }: IInfoProps){
 	return (
 		<div className={style.Info}>
-			<h4>濕度</h4>
-			<h4>{humidity}</h4>
-			<h4>本日最高溫度</h4>
-			<h4>{maxTemp}</h4>
-			<h6>{maxTempTime}</h6>
-			<h4>本日最低溫度</h4>
-			<h4>{minTemp}</h4>
-			<h6>{minTempTime}</h6>
-			<h4>氣壓</h4>
-			<h4>{pressure}</h4>
+			<h3>濕度</h3>
+			<h3>{humi(humidity)}</h3>
+			<h3>本日最高溫度</h3>
+			<h3>{temperature(maxTemp)}</h3>
+			<h5>{addColon(maxTempTime)}</h5>
+			<h3>本日最低溫度</h3>
+			<h3>{temperature(minTemp)}</h3>
+			<h5>{addColon(minTempTime)}</h5>
+			<h3>氣壓</h3>
+			<h3>{pres(pressure)}</h3>
 		</div>
 	)
 }
