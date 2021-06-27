7d8614a3fb5a053c794a2158ebb8347a9281e661
simba-fs <simba.fs@gmail.com>
2021-06-27 08:51:57 +0800

    fix wind direction

--- component/WeatherDisplay/widget/Wind/index.tsx
+++ component/WeatherDisplay/widget/Wind/index.tsx
@@ -6,12 +6,13 @@ interface IWindProps {
 }
 
 export default function Wind({speed, direction}: IWindProps){
+	console.log(direction, parseInt(direction) - 180);
 	return (
 		<>
 			<svg className={style.windDir} width="210" height="210" xmlns="http://www.w3.org/2000/svg">
 				<g>
 					<title>Wind Direction</title>
-					<g transform={`rotate(${direction}, 105, 105)`} stroke-width="3" id="svg_5">
+					<g transform={`rotate(${parseInt(direction) - 180}, 105, 105)`} stroke-width="3" id="svg_5">
 						<ellipse stroke-width="3" ry="100" rx="100" id="svg_1" cy="105" cx="105" fill="#fff" />
 						<line stroke-width="3" id="svg_2" y2="169" x2="105" y1="41" x1="105" fill="none" />
 						<line stroke-width="3" id="svg_3" y2="71" x2="135" y1="41" x1="105" fill="none" />
